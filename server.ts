714216e109d2a73d39a62ddf3ec8fd85a2e3af3f
Wito Delnat <wito.delnat@gmail.com>
2020-05-25 09:57:54 +0200

    Simplify

--- server.ts
+++ server.ts
@@ -1,40 +1,20 @@
 import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { LokiService } from "./loki/service.ts";
-import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
-const {
-  PORT,
-  LOKI_HOST,
-  LOKI_PORT,
-  LOKI_LABEL,
-  LOKI_FLUSH_INTERVAL,
-  LOKI_FLUSH_MAX_SIZE,
-} = config();
+const PORT = Deno.env.get("PORT") ?? "8080";
+const LABEL = Deno.env.get("LABEL");
 
 (async () => {
-  const loki = new LokiService({
-    host: LOKI_HOST,
-    port: LOKI_PORT,
-    labels: {
-      gateway: v4.generate(),
-      label: LOKI_LABEL,
-    },
-    flush: {
-      interval: Number(LOKI_FLUSH_INTERVAL),
-      maxSize: Number(LOKI_FLUSH_MAX_SIZE),
-    },
-  });
-
   const app = new expressive.App();
-  app.use(expressive.simpleLog());
   app.use(expressive.bodyParser.json());
 
   app.get("/healthz", async (_, res) => {
     await res.json([{ status: "pass" }]);
   });
   app.post("/log", async (req, res) => {
-    loki.log(req.data);
+    console.log({
+      label: LABEL,
+      ...req.data,
+    });
     await res.json([{ status: "ok" }]);
   });
 
